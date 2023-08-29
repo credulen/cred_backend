f0f38cc42e73deb8204d0b38906f9225eceb773e
profgreatwonder <muoghaluchidinma@gmail.com>
2023-08-29 04:21:41 +0100

    updated database.js file with ssl set to false

--- config/database.js
+++ config/database.js
@@ -103,7 +103,8 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", false),
+      // ssl: env.bool("DATABASE_SSL", false),
+      ssl: { rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false) },
     },
   },
 });
