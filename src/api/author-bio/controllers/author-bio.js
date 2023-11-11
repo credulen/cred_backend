0bfeab250cec2aedee0eac45c965844cb86926ff
profgreatwonder <muoghaluchidinma@gmail.com>
2023-11-11 17:37:52 +0100

    created new user info and updated the relation

---
+++ src/api/author-bio/controllers/author-bio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * author-bio controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::author-bio.author-bio');
---
+++ src/api/author-bio/services/author-bio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * author-bio service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::author-bio.author-bio');
